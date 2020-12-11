ac64d79412db2e7918a0076905c36801dc9ddd48
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2020-12-11 15:41:19 +0100

    add isAuth for login/logout, add basic navigation

--- Gifter/ClientApp/src/components/Home.js
+++ Gifter/ClientApp/src/components/Home.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-import LoginButton from '../auth/LoginButton/LoginButton';
-import LogoutButton from '../auth/LogoutButton/LogoutButton';
 
 export class Home extends Component {
   render() {
-    return (
-      <div>
-        <LoginButton />
-        <LogoutButton />
-      </div>
-    );
+    return <div></div>;
   }
 }
---
+++ Gifter/ClientApp/src/components/UI/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
+import LoginButton from '../../../auth/LoginButton/LoginButton';
+import LogoutButton from '../../../auth/LogoutButton/LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Navigation = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <h2 style={{ textAlign: 'center', margin: '20px' }}>Loading...</h2>;
+  }
+
+  return (
+    <div className={classes.Navigation}>
+      <div>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/fetch-data">Fetch Data</NavLink>
+        <NavLink to="/Counter">Counter</NavLink>
+      </div>
+      <div>{!isAuthenticated ? <LoginButton /> : <LogoutButton />}</div>
+    </div>
+  );
+};
+
+export default Navigation;
