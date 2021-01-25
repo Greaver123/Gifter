3e27b12926c9ec85c20e697730b5303e467d9b9a
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-01-25 12:30:30 +0100

    add getSubjectId extensions method to retrive userId from token

---
+++ Gifter/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Gifter.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string SubjectId(this ClaimsPrincipal user) { return user?.Claims?.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier, StringComparison.OrdinalIgnoreCase))?.Value; }
+
+    }
+}
