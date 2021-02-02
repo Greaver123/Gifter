234dd4438e35758d2023daf8c1cfbd3fc963f96f
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-02-02 14:21:03 +0100

    add Gifter,Services.Test project with sql lite in memory config

---
+++ Gifter.Services.Tests/GifterDBContextTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Gifter.Services.Tests
+{
+    [TestClass]
+    public class GifterDBContextTests: TestWithSQLiteBase
+    {
+        [TestMethod]
+        public async Task DatabaseIsAvailableAndCanBeConnectedTo()
+        {
+            Assert.IsTrue(await DbContext.Database.CanConnectAsync());
+        }
+    }
+}
---
+++ Gifter.Services.Tests/TestWithSQLiteBase.cs
@@ -0,0 +1,30 @@
+﻿using Gifter.DataAccess;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Gifter.Services.Tests
+{
+    public abstract class TestWithSQLiteBase: IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly GifterDbContext DbContext;
+
+        protected TestWithSQLiteBase()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<GifterDbContext>().UseSqlite(_connection).Options;
+
+            DbContext = new GifterDbContext(options);
+            DbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+}
