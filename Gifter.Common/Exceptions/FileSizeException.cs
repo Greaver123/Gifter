3bc96f4eaf4f182e14fd3e1ee86e17dec5aeb63c
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-02-08 15:37:00 +0100

    add FileSizeEception custom exception, add generic OperationResult class

---
+++ Gifter.Common/Exceptions/FileSizeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gifter.Common.Exceptions
+{
+    public class FileSizeException : Exception
+    {
+        public int MaxSize { get; private set; }
+
+        public FileSizeException(string message, int maxSize) : base(message)
+        {
+            MaxSize = maxSize;
+        }
+    }
+}
--- Gifter.Services/Common/OperationResult.cs
+++ Gifter.Services/Common/OperationResult.cs
@@ -1,11 +1,17 @@
 ﻿using Gifter.Services.Constants;
+using System.Collections.Generic;
 
 namespace Gifter.Services.Common
 {
     public class OperationResult<T>
     {
+        public OperationResult()
+        {
+            Errors = new List<OperationError>();
+        }
         public string Message { get; internal set; }
         public OperationStatus Status { get; internal set; }
         public T Payload { get; internal set; }
+        public IList<OperationError> Errors { get; set; }
     }
 }
