af48b46897a5f718948399fe3e21a1a529d9fbad
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-02-12 16:23:52 +0100

    Remove required attribute from Wish name, delete imageUrl property

--- Gifter.DataAccess/Models/Wish.cs
+++ Gifter.DataAccess/Models/Wish.cs
@@ -6,14 +6,11 @@ namespace Gifter.DataAccess.Models
     {
         public int Id { get; set; }
         
-        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string URL { get; set; }
-
-        public string ImageUrl { get; set; }
 
         [DataType(DataType.Currency)]
         public double Price { get; set; }
