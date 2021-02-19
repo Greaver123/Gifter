5cc03f57817f81dea93ee5ee80517c8495f8002f
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-02-19 17:53:09 +0100

    change MessageHelper to be more generic, add ErrorMessage and SuccessMessage methods

--- Gifter.Services/Helpers/MessageHelper.cs
+++ Gifter.Services/Helpers/MessageHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gifter.Services.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,36 @@ namespace Gifter.Services.Helpers
 {
     public static class MessageHelper
     {
+
         /// <summary>
         /// Message entity not found for given id and entity type.
         /// </summary>
-        /// <param name="entityType">Type of entity.</param>
+        /// <param name="entityName">Name of entity</param>
         /// <param name="id">Id of entity</param>
-        /// <returns>Message: "<paramref name="entityType"/> with id = <paramref name="id"/> not found."</returns>
-        public static string EntityNotFoundMessage(Type entityType, int id)
+        /// <returns>Message: "<paramref name="entityName"/> with id = <paramref name="id"/> not found."</returns>
+        public static string CreateEntityNotFoundMessage(string entityName, int id)
         {
-            return $"{nameof(Type)} with id = {id} not found.";
+            return $"{entityName} with id = {id} not found.";
+        }
+
+        /// <summary>
+        /// Create operation "Error" message for given entity and operation type.
+        /// </summary>
+        /// <param name="entityName">Name of entity.</param>
+        /// <returns>Message: Internal error. Could not <paramref name="operationType"/> "<paramref name="entityName"/>".</returns>
+        public static string CreateOperationErrorMessage(string entityName, OperationType operationType)
+        {
+            return $"Internal error. Could not {operationType} \"{entityName}\".";
+        }
+
+        /// <summary>
+        /// Create operation "success" message for given entity and operation type.
+        /// </summary>
+        /// <param name="entityName">Name of entity.</param>
+        /// <returns>Message: "<paramref name="entityName"/>" updated successful.</returns>
+        public static string CreateOperationSuccessMessage(string entityName, OperationType operationType)
+        {
+            return $"\"{entityName}\" {operationType} successful.";
         }
     }
 }
