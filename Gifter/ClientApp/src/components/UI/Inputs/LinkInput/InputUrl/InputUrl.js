80e735a52abd9f7dd67bf663f95fd7deac24cacb
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-03-10 15:14:18 +0100

    further refactor for LinkInput, add focusIn, FocusOut on Enter/Escape

--- Gifter/ClientApp/src/components/UI/Inputs/LinkInput/InputUrl/InputUrl.js
+++ Gifter/ClientApp/src/components/UI/Inputs/LinkInput/InputUrl/InputUrl.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './InputUrl.module.css';
+import InputToobar from './InputToolbar/InputToolbar';
+import Link from './Link/Link';
 
 const InputUrl = (props) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (props.editMode) {
+      console.log('EditMODE');
+      inputRef.current.focus();
+    }
+  });
+
+  const keyEventsHandle = function (e) {
+    if (e.key === 'Enter') {
+      props.onSaveClick();
+    } else if (e.key === 'Escape') {
+      props.onCancel();
+    }
+  };
+
   return (
-    <input
-      className={`${classes.UrlInput}`}
-      type="url"
-      value={props.url ?? ''}
-      placeholder="Enter URL"
-      pattern="https://.*"
-      name="link"
-      onBlur={props.onFocusOut}
-      onChange={props.onChange}
-    />
+    <div className={classes.LinkInput}>
+      <React.Fragment>
+        <input
+          ref={inputRef}
+          className={`${classes.UrlInput} ${
+            !props.editMode ? classes.Hidden : ''
+          }`}
+          type="url"
+          value={props.url ?? ''}
+          placeholder="Enter URL"
+          pattern="https://.*"
+          name="link"
+          onBlur={props.onFocusOut}
+          onChange={props.onChange}
+          onKeyDown={keyEventsHandle}
+        />
+        {!props.editMode ? <Link url={props.url ?? 'Enter URL'} /> : null}
+      </React.Fragment>
+      <InputToobar
+        editMode={props.editMode}
+        onSaveClick={props.onSaveClick}
+        onEditClick={() => {
+          props.onEditClick();
+          inputRef.current.focus();
+        }}
+        onDeleteClick={props.onDeleteClick}
+      />
+    </div>
   );
 };
 
--- Gifter/ClientApp/src/components/UI/Inputs/LinkInput/LinkInput.js
+++ Gifter/ClientApp/src/components/UI/Inputs/LinkInput/LinkInput.js
@@ -1,58 +1,25 @@
 import React from 'react';
 import classes from './LinkInput.module.css';
 import { useState, useEffect, useRef } from 'react';
-import InputToobar from './InputToolbar/InputToolbar';
 import InputUrl from './InputUrl/InputUrl';
-import Link from './Link/Link';
 import LinkButton from './LinkButton/LinkButton';
 
 const LinkInput = (props) => {
   const [isVisibleEditWindow, setIsVisibleEditWindow] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
-  const urlInput = useRef(null);
+  const inputUrlRef = useRef(null);
   const linkInputWrapper = useRef(null);
 
-  // useEffect(() => {
-  //   const closeEditWindow = function (e) {
-  //     console.log('close');
-  //     setIsEditMode(false);
-  //     setIsVisibleEditWindow(false);
-  //   };
-
-  //   const closeEditWindowOnClick = function (e) {
-  //     console.log(isVisibleEditWindow);
-  //     console.dir(e.target);
-
-  //     // const target = e.target.contains(urlInput.current);
-  //     // console.log(target);
-
-  //     console.log(e.target.closest(`.${classes.InputLink}`));
-  //     if (
-  //       isVisibleEditWindow &&
-  //       e.target.closest(`.${classes.InputLink}`) === null
-  //     ) {
-  //       closeEditWindow();
-  //       console.log('close');
-  //     }
-  //   };
-
-  //   console.log('Use Effect');
-
-  //   document.addEventListener('keydown', closeEditWindow);
-  //   document.addEventListener('click', closeEditWindowOnClick);
-
-  //   return () => {
-  //     document.removeEventListener('keydown', escape);
-  //     document.removeEventListener('click', closeEditWindowOnClick);
-  //   };
-  // }, [linkInput, isVisibleEditWindow]);
+  useEffect(() => {
+    console.log('[ComponentDidMount] LinkInput');
+    console.log(inputUrlRef);
+  }, [inputUrlRef]);
 
   const showEditWindow = function () {
     setIsVisibleEditWindow(true);
   };
 
   const startEditMode = function () {
-    console.log(urlInput);
     setIsEditMode(true);
   };
 
@@ -64,49 +31,32 @@ const LinkInput = (props) => {
 
   const deleteLink = function (e) {
     props.onDeleteLink();
-    setIsEditMode(true);
+    setIsEditMode(false);
+    setIsVisibleEditWindow(false);
   };
 
-  const inputUrl = (
-    <InputUrl
-      url={props.url}
-      onFocusOut={() => {
-        console.log('focus out');
-        setIsEditMode(false);
-        setIsVisibleEditWindow(false);
-      }}
-      onChange={(e) => {
-        props.onChange(e);
-      }}
-    />
-  );
-
-  const linkInput = (
-    <div className={classes.LinkInput}>
-      {isEditMode ? (
-        inputUrl
-      ) : props.url === null ? (
-        inputUrl
-      ) : (
-        <Link url={props.url} />
-      )}
-      {isVisibleEditWindow ? (
-        <InputToobar
-          editMode={isEditMode}
-          onDeleteClick={deleteLink}
-          onEditClick={startEditMode}
-          onSaveClick={saveLink}
-        />
-      ) : null}
-    </div>
-  );
+  const cancelEdit = function (e) {
+    setIsEditMode(false);
+    setIsVisibleEditWindow(false);
+  };
 
   return (
     <div ref={linkInputWrapper} className={classes.LinkInputWrapper}>
       {isVisibleEditWindow ? (
-        linkInput
+        <InputUrl
+          url={props.url}
+          onFocusOut={cancelEdit}
+          onChange={(e) => {
+            props.onChange(e);
+          }}
+          onSaveClick={saveLink}
+          onEditClick={startEditMode}
+          onDeleteClick={deleteLink}
+          onCancel={cancelEdit}
+          editMode={isEditMode}
+        />
       ) : (
-        <LinkButton url={props.url} onClick={showEditWindow} />
+        <LinkButton url={props.url ?? 'Enter URL'} onClick={showEditWindow} />
       )}
     </div>
   );
