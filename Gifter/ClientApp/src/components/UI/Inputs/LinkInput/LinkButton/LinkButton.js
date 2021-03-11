4b23a7ff9b204c68ea67ecd560d8168113383728
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-03-11 16:59:11 +0100

    add slideToSide effect for LinkInput

--- Gifter/ClientApp/src/components/UI/Inputs/LinkInput/LinkButton/LinkButton.js
+++ Gifter/ClientApp/src/components/UI/Inputs/LinkInput/LinkButton/LinkButton.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './LinkButton.module.css';
 
 const LinkButton = (props) => {
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <div className={classes.LinkButton} onClick={props.onClick}>
+    <div
+      className={`${classes.LinkButton} ${clicked ? classes.Clicked : ''}`}
+      onClick={() => setClicked((prevState) => !prevState)}
+      onTransitionEnd={props.onClick}
+    >
       {props.url?.replace(/^((http)|(https))(:\/\/)/, '')}
     </div>
   );
